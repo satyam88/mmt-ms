29e033c4575dc8f4c366dd080bb5385672b170c9
“Satyam <satyam@gmail.com>
2023-12-10 12:03:39 +0530

    adding hotel.java

--- src/main/java/com/example/mmtms/hotels.java
+++ src/main/java/com/example/mmtms/hotels.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class flights {
-    @GetMapping("/flights")
-    public String getData() {return  "Please book flight at 15% discount" ; }
+public class hotels {
+    @GetMapping("/hotels")
+    public String getData() {return  "Please book hotels at 20% discount from mmt" ; }
 }
