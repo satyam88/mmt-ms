caaa5175dcbb349851eca2c1c955d9b3aa73a368
“Satyam <satyam@gmail.com>
2023-12-16 11:04:36 +0530

    adding holidsya

---
+++ src/main/java/com/example/mmtms/holidays.java
@@ -0,0 +1,10 @@
+package com.example.mmtms;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class holidays {
+    @GetMapping("/holidays")
+    public String getData() {return  "Please book holidays from MMT at 15% discount" ; }
+}
