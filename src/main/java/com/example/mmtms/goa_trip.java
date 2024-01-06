6b9dea15e6954b7499303b82ac19c1c2fa79b45b
“Satyam <satyam@gmail.com>
2024-01-06 11:18:19 +0530

    adding code block for

---
+++ src/main/java/com/example/mmtms/goa_trip.java
@@ -0,0 +1,10 @@
+package com.example.mmtms;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class goa_trip {
+    @GetMapping("/goa_trip")
+    public String getData() {return  "Please book holidays from MMT at 35% discount" ; }
+}
