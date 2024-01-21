6e7d4557e8ddb4895c392a32d5fba9400c424f6d
“Satyam <satyam@gmail.com>
2024-01-21 11:41:35 +0530

    hello

--- src/main/java/com/example/mmtms/goa_trip.java
+++ src/main/java/com/example/mmtms/goa_trip.java
@@ -6,5 +6,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class goa_trip {
     @GetMapping("/goa_trip")
-    public String getData() {return  "Please book holidays from MMT at 35% discount" ; }
+    public String getData() {return  "Please book holidays from MMT at 55% discount" ; }
 }
